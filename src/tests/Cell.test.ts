7cb6b23e251b49979b6cfe5bb188a741b3f23b45
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-12-21 02:03:39 +0100

    test: update tests for Cell

--- src/tests/Cell.test.ts
+++ src/tests/Cell.test.ts
@@ -1,7 +1,8 @@
 import { Cell, CellState } from '../core/Cell';
 
 const UNDERPOPULATION = 1;
-const OVERCROWDING = 3;
+const OVERCROWDING = 4;
+const REBIRTH = 3;
 const STABLE = [2, 3];
 
 describe('a cell', () => {
@@ -9,27 +10,26 @@ describe('a cell', () => {
     const cell = new Cell(CellState.ALIVE);
     expect(cell).toBeInstanceOf(Cell);
   });
-
   it('alive with fewer than two live neighbours dies, as if caused by underpopulation', () => {
     const cell = new Cell(CellState.ALIVE);
-    cell.nextState(UNDERPOPULATION);
-    expect(cell.isAlive()).toBe(false);
+    const nextState = cell.nextState(UNDERPOPULATION);
+    expect(nextState).toBe(CellState.DEAD);
   });
   it('alive with with more than three live neighbours dies, as if by overcrowding', () => {
     const cell = new Cell(CellState.ALIVE);
-    cell.nextState(OVERCROWDING);
-    expect(cell.isAlive()).toBe(false);
+    const nextState = cell.nextState(OVERCROWDING);
+    expect(nextState).toBe(CellState.DEAD);
   });
   it('alive with two or three live neighbours lives on to the next generation', () => {
     const cell = new Cell(CellState.ALIVE);
     STABLE.forEach((numberOfAliveNeighbours) => {
-      cell.nextState(numberOfAliveNeighbours);
-      expect(cell.isAlive()).toBe(true);
+      const nextState = cell.nextState(numberOfAliveNeighbours);
+      expect(nextState).toBe(CellState.ALIVE);
     });
   });
   it('dead with exactly three live neighbours becomes a live cell', () => {
     const cell = new Cell(CellState.DEAD);
-    cell.nextState(3);
-    expect(cell.isAlive()).toBe(true);
+    const nextState = cell.nextState(REBIRTH);
+    expect(nextState).toBe(CellState.ALIVE);
   });
 });
