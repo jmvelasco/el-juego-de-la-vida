e90a1cd5abe39ba18a9ff5e9e4f73c88e5078723
J. Manuel Velasco <jmanuel.velasco@gmail.com>
2025-12-22 23:54:34 +0100

    refactor: simplify nextState logic

--- src/core/Cell.ts
+++ src/core/Cell.ts
@@ -7,14 +7,18 @@ export class Cell {
   constructor(private state: CellState) {}
 
   nextState(numberOfAliveNeighbours: number) {
-    if (this.isAlive() && numberOfAliveNeighbours === 2) {
-      return new Cell(CellState.ALIVE);
-    } else if (this.isAlive() && (numberOfAliveNeighbours < 2 || numberOfAliveNeighbours > 3)) {
-      return new Cell(CellState.DEAD);
-    } else if (!this.isAlive() && numberOfAliveNeighbours === 3) {
-      return new Cell(CellState.ALIVE);
-    }
-    return this.isAlive() ? new Cell(CellState.ALIVE) : new Cell(CellState.DEAD);
+    return (this.isAlive() && this.stable(numberOfAliveNeighbours)) ||
+      (!this.isAlive() && this.reborn(numberOfAliveNeighbours))
+      ? new Cell(CellState.ALIVE)
+      : new Cell(CellState.DEAD);
+  }
+
+  private stable(numberOfAliveNeighbours: number) {
+    return numberOfAliveNeighbours === 2 || numberOfAliveNeighbours === 3;
+  }
+
+  private reborn(numberOfAliveNeighbours: number) {
+    return numberOfAliveNeighbours === 3;
   }
 
   isAlive() {
